08937da20a32766a96144140390d960b1fcb6eba
Shaurya Bhandari <shauryabhandari25@gmail.com>
2020-09-25 23:23:49 +0530

    Home done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
+import { Layout } from "antd";
+import AppHeader from "./Components/Common/AppHeader";
+import Home from "./Components/Home/Home";
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Layout className="mainLayout">
+        <Header>
+          <AppHeader />
+        </Header>
+        <Content>
+          <Home />
+        </Content>
+        {/* <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer> */}
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/About/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="block aboutBlock">
+      <div className="container-fluid">
+        <div className="titleHolder">
+          <h2>About Us</h2>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className="contentHolder">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+            nobis molestias? Minima fuga nemo deserunt dolorem, sed dolor illo,
+            blanditiis at quod assumenda enim! Magni harum neque facilis
+            mollitia, nihil quidem veniam porro, autem molestiae, ab iste? Sunt
+            culpa sapiente vero, delectus, suscipit numquam nulla nemo laborum
+            cupiditate harum laudantium.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
