2dc6aa91249aa27f27fd027a3ca03d3a6a746025
Shaurya Bhandari <shauryabhandari25@gmail.com>
2022-03-12 22:04:27 +0530

    hello

--- backend/server.py
+++ backend/server.py
@@ -14,30 +14,32 @@ def get_time():
 
     # Returning an api for showing in reactjs
     return {
-        'Name': "geek",
-        "Age": "22",
-        "Date": x,
-        "programming": "python"
+        # 'Name': "geek",
+        # "Age": "22",
+        # "Date": x,
+        # "programming": "python"
     }
 
 # Route for sending and receiving image
-@app.route('/maskImage' , methods=['POST'])
-@app.route('/test' , methods=['POST'])
+
+
+@app.route('/maskImage', methods=['POST'])
+@app.route('/test', methods=['POST'])
 def mask_image():
-	# print(request.files , file=sys.stderr)
-	print("called")
-	file = request.files['image'].read() ## byte file
-	npimg = np.fromstring(file, np.uint8)
-	img = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
-	new_img = detectByImage(img)
-	new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2RGB)
-	new_img = Image.fromarray(new_img.astype("uint8"))
-	rawBytes = io.BytesIO()
-	new_img.save(rawBytes, "JPEG")
-	rawBytes.seek(0)
-	img_base64 = base64.b64encode(rawBytes.read())
-	# print(img_base64)
-	return jsonify({'status':str(img_base64)})
+    # print(request.files , file=sys.stderr)
+    print("called")
+    file = request.files['image'].read()  # byte file
+    npimg = np.fromstring(file, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    new_img = detectByImage(img)
+    new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2RGB)
+    new_img = Image.fromarray(new_img.astype("uint8"))
+    rawBytes = io.BytesIO()
+    new_img.save(rawBytes, "JPEG")
+    rawBytes.seek(0)
+    img_base64 = base64.b64encode(rawBytes.read())
+    # print(img_base64)
+    return jsonify({'status': str(img_base64)})
 
 
 # Running app
